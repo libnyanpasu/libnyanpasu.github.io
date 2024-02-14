7891bc3797e4f039b8b0f6e5e7287aeb98d6e265
greenhat616 <i@a632079.me>
2024-02-15 02:05:06 +0800

    fix: lint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,14 @@
 export default {
   '**/*.vue': [
+    'prettier --write',
     'eslint -c .eslintrc.cjs',
     'stylelint --config stylelint.config.js'
   ],
-  '**/*.{ts,tsx,js,jsx}': ['eslint -c .eslintrc.cjs'],
+  '**/*.{ts,tsx,js,jsx}': ['eslint -c .eslintrc.cjs', 'prettier --write'],
   '**/*.{ts,tsx,vue}': [() => 'tsc -p tsconfig.json --noEmit'],
-  '**/*.{scss,css,sass}': ['stylelint --config stylelint.config.js'],
+  '**/*.{scss,css,sass}': [
+    'stylelint --config stylelint.config.js',
+    'prettier --write'
+  ],
   '**/*.{json,md}': ['prettier --write']
 }
